eceb44bab5653c8d80884ccb91021a2795ffa9a0
Darshan P H <phdarshan@altimetrik.com>
2023-12-21 19:55:28 +0530

    Bug Fix

--- DarshanPH_AIPL18239/schedule-microservice/src/main/java/com/altimetrik/schedulemicroservice/model/ScheduleInput.java
+++ DarshanPH_AIPL18239/schedule-microservice/src/main/java/com/altimetrik/schedulemicroservice/model/ScheduleInput.java
@@ -1,7 +1,6 @@
 package com.altimetrik.schedulemicroservice.model;
 
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,10 +18,8 @@ public class ScheduleInput {
         private int trainId;
         private int routeId;
         @NotNull(message = "Please enter the Train schedule departure date")
-        @FutureOrPresent(message = "Please enter the future or present date for doe")
         private LocalDateTime departureDateTime;
         @NotNull(message = "Please enter the Train schedule arrival date")
-        @FutureOrPresent(message = "Please enter the future or present date for doe")
         private LocalDateTime arrivalDateTime;
     }
 
